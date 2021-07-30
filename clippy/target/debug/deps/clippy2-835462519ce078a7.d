9d1cf37798176d4a38afc172b8a09dafb3796322
Ishmaev, Daniel <dan.samara@gmail.com>
2021-07-30 18:33:06 +0400

    Version 4.5.0

---
+++ clippy/target/debug/deps/clippy2-835462519ce078a7.d
@@ -0,0 +1,7 @@
+/home/dan/projects/rustlings/exercises/clippy/target/debug/deps/clippy2-835462519ce078a7.rmeta: clippy2.rs
+
+/home/dan/projects/rustlings/exercises/clippy/target/debug/deps/clippy2-835462519ce078a7.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
